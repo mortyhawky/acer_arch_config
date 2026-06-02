79a8e120bd193c5cf43a525d5f45cd32163395c0
mortyhawky <morty.hawky@outlook.com>
2026-06-02 11:16:48 +0200

    2026-06-02T11:16:48+02:00

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,5 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
-
--- Use config_builder if available (newer wezterm versions)
---if wezterm.config_builder then
---  config = wezterm.config_builder()
---end
 
 -- === Core ===
 config.default_prog = { "bash", "--login" }   -- or zsh, fish, etc.
@@ -13,14 +7,16 @@ config.enable_tab_bar = false                 -- minimal UI
 config.window_decorations = "NONE"            -- no titlebar (WM handles it)
 
 -- === Font ===
-config.font = wezterm.font("AdwaitaMono Nerd Font Mono")
---config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+--config.font = wezterm.font("AdwaitaMono Nerd Font Mono")
 --config.font = wezterm.font("DejaVuSansM Nerd Font Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 11.0
 
 -- === Appearance ===
-config.color_scheme = "Builtin Solarized Dark"
-config.window_background_opacity = 1.0
+config.color_scheme = "3024 (base16)"
+--config.color_scheme = "Builtin Solarized Dark"
+--config.color_scheme = "Builtin Batman"
+config.window_background_opacity = 0.8
 
 -- === Behavior ===
 config.scrollback_lines = 5000
@@ -32,23 +28,3 @@ config.keys = {
 }
 
 return config
---""-- -- Pull in the wezterm API
---""-- local wezterm = require "wezterm"
---""-- 
---""-- -- This will hold the configuration.
---""-- local config = wezterm.config_builder()
---""-- 
---""-- -- This is where you actually apply your config choises.
---""-- 
---""-- config.font_size = 11
---""-- 
---""-- --config.font = wezterm.font "AdwaitaMono Nerd Font Mono"
---""-- config.font = wezterm.font "JetBrainsMono Nerd Font"
---""-- --font_family = wezterm.font "DejaVuSansM Nerd Font Mono"
---""-- 
---""-- --config.color_scheme = "AdventureTime"
---""-- --config.color_scheme = "Batman"
---""-- config.color_scheme = "GruvboxDark"
---""-- 
---""-- -- Finally, return the configuration to wezterm
---""-- return config
