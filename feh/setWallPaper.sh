b3611adaffcf354d565aaa4accea643d27bfe66d
mortyhawky <morty.hawky@outlook.com>
2026-06-19 04:53:19 +0200

    2026-06-19T04:52:07+02:00

--- feh/setWallPaper.sh
+++ feh/setWallPaper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 feh --no-fehbg --bg-fill \
-    $XDG_CONFIG_HOME/wallpapers/wp1600x900base16-3024-rectangles.png
+    $XDG_CONFIG_HOME/wallpapers/wp-atelier-seaside-full16-claude.png
