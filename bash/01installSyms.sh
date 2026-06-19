ee85c84c18b9b9b762f686894f141100f6a9a172
mortyhawky <morty.hawky@outlook.com>
2026-06-19 02:09:08 +0200

    2026-06-19T02:09:03+02:00

--- bash/01installSyms.sh
+++ bash/01installSyms.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-
+# Create symlinks to XDG_CONFIG_HOME
 ln -fs ~/.config/bash/bashrc          ~/.bashrc
 ln -fs ~/.config/bash/bash_profile    ~/.bash_profile
 
-ls ~ -Al --color=always | grep -E --color=always "^l"
+# Confirm symlinks
+ls ~ -AlG --color=always | grep -E --color=always "^l"
