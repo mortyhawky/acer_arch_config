fff4fa0407dda06ae316a448ad41092becb8f440
mortyhawky <morty.hawky@outlook.com>
2026-06-21 08:02:19 +0200

    2026-06-21T08:01:40+02:00

--- feh/setWallPaper.sh
+++ feh/setWallPaper.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 feh --no-fehbg --bg-fill \
-    $XDG_CONFIG_HOME/wallpapers/wp-atelier-seaside-full16-claude.png
+    $XDG_CONFIG_HOME/wallpapers/atelier_seaside02.png
--- nvim/lua/phaeno/keymaps.lua
+++ nvim/lua/phaeno/keymaps.lua
@@ -2,18 +2,27 @@
 -- KEYMAPS
 -- =====================================
 
+-- Toggle line numbers with Ctrl n
+vim.keymap.set("n", "<C-n>", function()
+  if vim.wo.number then
+    vim.wo.number         = false
+    vim.wo.relativenumber = false
+  else
+    vim.wo.number         = true
+    vim.wo.relativenumber = true
+  end
+end)
 
+-- Save & Exit
 -- Write all changed buffers and exit
-vim.keymap.set( "n",        "ZZ",        ":wqall<CR>" )
---vim.keymap.set( "i",        "<A-p>"      
+vim.keymap.set( {"n"},         "ZZ",         ":wqall<CR>" )
 
-vim.keymap.set( {"n","i"}, "<C-s>",      "<Esc><Cmd>:write<CR><Cmd>:nohls<CR>" )
-vim.keymap.set(  "n",      "<leader>so", "<Cmd>:source<CR><Cmd>:nohls<CR>:echo 'Sourced'<CR>" )
-vim.keymap.set( {"n","i"}, "<Esc>",      "<Esc><Cmd>:nohls<CR>")
---vim.keymap.set( {"n","i"}, "C-c",        "<Esc><Cmd>:nohls<CR>:echo 'Esc'<CR>'" )
+vim.keymap.set( {"n","i","v"}, "<C-s>",      "<Esc><Cmd>:write<CR><Cmd>:nohls<CR>" )
+vim.keymap.set( {"n"},         "<leader>so", "<Cmd>:source<CR><Cmd>:nohls<CR>:echo 'Sourced'<CR>" )
+vim.keymap.set( {"n","i"},     "<Esc>",      "<Esc><Cmd>:nohls<CR>")
 
--- Yank form to cursor to end of line
-vim.keymap.set(  "n",      "Y",     "y$")
+-- Yank from cursor position to end of line
+vim.keymap.set( {"n"},         "Y",          "y$")
 
 -- Replaces selected text WITHOUT
 -- losing what you yanked
@@ -23,8 +32,8 @@ vim.keymap.set( "x", "p", [["_dP]] )
 vim.keymap.set( {"n","v"}, "<leader>d", [["_d]] )
 
 -- Moves line(s) up or down in visual selection mode
-vim.keymap.set( "v", "J", ":m '>+1<CR>gv=gv" )
-vim.keymap.set( "v", "K", ":m '<-2<CR>gv=gv" )
+    vim.keymap.set( "v", "J", ":m '>+1<CR>gv=gv" )
+    vim.keymap.set( "v", "K", ":m '<-2<CR>gv=gv" )
 
 -- Indent and keep selection
 vim.keymap.set( "v", "<", ">gv" )
