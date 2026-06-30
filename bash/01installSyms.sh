bbbdc2630b5e42b17ec31abb27512cc78477bc0b
mortyhawky <morty.hawky@outlook.com>
2026-06-30 20:04:14 +0200

    2026-06-30T20:02:25+02:00

--- bash/01installSyms.sh
+++ bash/01installSyms.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # Create symlinks to XDG_CONFIG_HOME
-ln -fs ~/.config/bash/bashrc          ~/.bashrc
-ln -fs ~/.config/bash/bash_profile    ~/.bash_profile
-ln -fs ~/.config/bash/bash_logout     ~/.bash_logout
+ln -fsv ~/.config/bash/bashrc          ~/.bashrc
+ln -fsv ~/.config/bash/bash_profile    ~/.bash_profile
+ln -fsv ~/.config/bash/bash_logout     ~/.bash_logout
+ln -fsv ~/.config/readline             ~/.inputrc
 
 # Confirm symlinks
 ls ~ -AlG --color=always | grep -E --color=always "^l"
