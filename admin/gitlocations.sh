0e9443d586fe8d310bb4eb6b4b6dcfc3e2988263
mortyhawky <morty.hawky@outlook.com>
2026-09-09 17:53:09 +0200

    2026-09-09T17:52:00+02:00

--- admin/gitlocations.sh
+++ admin/gitlocations.sh
@@ -1,5 +1,43 @@
 #/usr/bin env bash
 
-cd ~/.config        && eza --long --git |bat
-cd /a/linux/arch    && eza --long --git |bat
-cd /a/prog          && eza --long --git |bat
+clear
+
+checkgit() {
+    pushd $folder
+        printf "    in folder: "; pwd
+        git status --short
+        printf "____----------------____\n\n"
+    popd
+}
+
+folder="/home/m/.config"
+checkgit
+folder="/a/prog"
+checkgit
+folder="/a/linux/arch"
+checkgit
+
+#folder="/a/prog"
+#pushd $folder
+#    printf "    in folder: "; pwd
+#    git status --short
+#popd
+#
+##printf "$folder\n"
+###git remote --verbose
+##git status --short
+##printf "___--------------------------------------___\n\n"
+##
+##cd /a/linux/arch
+##git status
+##git remote --verbose
+##git status --short
+##printf "___--------------------------------------___\n\n"
+##
+##cd /a/prog
+##git status
+##git remote --verbose
+##git status --short
+##printf "___--------------------------------------___\n\n"
+##
+##popd
